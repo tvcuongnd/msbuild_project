fad86597f3318c497dc1d0e7e6da4fc2f07d9249
shazforiot <shazforiot@gmail.com>
2020-06-07 11:35:46 +0530

    updates

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -2,11 +2,11 @@
 
 namespace ConsoleApp
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! from Thetips4you");
         }
     }
 }
--- ConsoleApp/UnitTestProject/UnitTest1.cs
+++ ConsoleApp/UnitTestProject/UnitTest1.cs
@@ -7,7 +7,7 @@ namespace UnitTestProject
     [TestClass]
     public class UnitTest1
     {
-        private const string Expected = "Hello World!";
+        private const string Expected = "Hello World! from Thetips4you";
         [TestMethod]
         public void TestMethod1()
         {
